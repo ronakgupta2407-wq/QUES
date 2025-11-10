083f50b44d5be26bf2cba54abe1feff229e69e8c
ronakgupta2407-wq <ronakgupta2407@gmail.com>
2025-11-10 23:25:13 +0530

    Add LENGTHOFSTRING.cpp to calculate string length
    Implement a program to calculate the length of a string.

---
+++ LENGTHOFSTRING.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char str[100];
+    int length; 
+    cout<<"Enter The String = ";
+    cin.getline(str,100);
+    
+    for(length = 0; str[length]!= '\0'; ++length){
+        
+    }
+    cout<<"LENGTH = "<<length<<endl;
+    
+}
